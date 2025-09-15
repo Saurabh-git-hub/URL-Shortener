33848804be6e528078c587046b9e20eb6aff61cb
Saurabh Chauhan <saurabh20002004@gmail.com>
2025-09-15 13:34:21 +0530

    Fix routing for React Router on Vercel

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,42 +1,46 @@
-import {storeClicks} from "@/db/apiClicks";
-import {getLongUrl} from "@/db/apiUrls";
-import useFetch from "@/hooks/use-fetch";
-import {useEffect} from "react";
-import {useParams} from "react-router-dom";
-import {BarLoader} from "react-spinners";
+// import {storeClicks} from "@/db/apiClicks";
+// import {getLongUrl} from "@/db/apiUrls";
+// import useFetch from "@/hooks/use-fetch";
+// import {useEffect} from "react";
+// import {useParams} from "react-router-dom";
+// import {BarLoader} from "react-spinners";
 
-const RedirectLink = () => {
-  const {id} = useParams();
+// const RedirectLink = () => {
+//   const {id} = useParams();
 
-  const {loading, data, fn} = useFetch(getLongUrl, id);
+//   const {loading, data, fn} = useFetch(getLongUrl, id);
 
-  const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
-    id: data?.id,
-    originalUrl: data?.original_url,
-  });
+//   const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+//     id: data?.id,
+//     originalUrl: data?.original_url,
+//   });
 
-  useEffect(() => {
-    fn();
-  }, []);
+//   useEffect(() => {
+//     fn();
+//   }, []);
 
-  useEffect(() => {
-    if (!loading && data) {
-      fnStats();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading]);
+//   useEffect(() => {
+//     if (!loading && data) {
+//       fnStats();
+      
+//     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [loading]);
 
-  if (loading || loadingStats) {
-    return (
-      <>
-        <BarLoader width={"100%"} color="#36d7b7" />
-        <br />
-        Redirecting...
-      </>
-    );
-  }
+//   if (loading || loadingStats) {
+//     return (
+//       <>
+//         <BarLoader width={"100%"} color="#36d7b7" />
+//         <br />
+//         Redirecting...
+//       </>
+//     );
+//   }
 
-  return null;
-};
+//   return null;
+// };
 
-export default RedirectLink;
+// export default RedirectLink;
+
+
+// my code 
